9c9138f0bc8a9dd783f44f2ca0e111f157fb2f17
Mustafa Guer <mustafaguer@outlook.com>
2021-09-09 16:10:12 +0200

    get plz input value

--- components/addressCard/address-card.class.js
+++ components/addressCard/address-card.class.js
@@ -5,25 +5,46 @@ addressTemplate.innerHTML = `
     </style>
 
     <div class="container">
-        <form>
-            <input placeholder="PLZ" type="number" >
-            <input placeholder="Stadt" type="string" >
-            <input placeholder="Straße" type="string" >
-            <input placeholder="Hausnummer" type="number" >
+        <form onsubmit="return false;" method="POST">
+            <input id="search_plz" min="0" placeholder="PLZ" type="number" >
+            <input id="search_city" placeholder="Stadt" type="text" >
+            <input id="search_street" placeholder="Straße" type="search" autocomplete="off">
+            
+            <input id="search_nr" min="1" placeholder="Hausnummer" type="text" >
             <input value="DE - Deutschland" disabled >
         </form>
     </div>
 `;
 
+const CORS_URL = "https://cors-anywhere.herokuapp.com/";
+const URL_Server = "https://www.postdirekt.de/plzserver/PlzAjaxServlet?";
+
 class AddressCard extends HTMLElement {
     constructor() {
         super();
 
-        this.attachShadow({ mode : 'open' });
+        this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(addressTemplate.content.cloneNode(true));
 
-
     }
+
+    async searchPlz() {
+
+        let response = await fetch(CORS_URL + URL_Server + "finda=city" + "&" + `city=${this.shadowRoot.querySelector('#search_plz').value}`);
+        console.log('response: ', response);
+        let responseAsJson = await response.json();
+        console.log('responseAsJson: ', responseAsJson);
+
+        console.log('input value: ', this.shadowRoot.querySelector('#search_plz').value);
+    }
+
+    connectedCallback() {
+        this.shadowRoot.querySelector('#search_plz').addEventListener('keyup', () => this.searchPlz());
+    }
+    disconnecteCallback() {
+        this.shadowRoot.querySelector('#search_plz').removeEventListener('keyup', () => this.searchPlz());
+    }
+
 }
 
 window.customElements.define("app-address-card", AddressCard);
